1f41431b5e428530ce9de8a095abba20c0ffeb30
armaan <armaanmathew88@gmail.com>
2020-10-28 18:22:27 +0530

    section 3 video 23

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello my name is Armaan Mathew!</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            string: "My name is Armaan Philip Mathew",
+            age: 27,
+        };
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <div>{this.state.string}</div>
+                    <p>{this.state.age}</p>
+                    <button
+                        onClick={() =>
+                            this.setState({
+                                string: "My brother's name is Rehaan",
+                            })
+                        }
+                    >
+                        Click Me
+                    </button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;

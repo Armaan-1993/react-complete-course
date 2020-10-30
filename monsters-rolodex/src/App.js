7b3a55fe81b861d973591b55a11c64120f8ce4af
armaan <armaanmathew88@gmail.com>
2020-10-30 23:37:10 +0530

    working with section 3 video 31

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.components.jsx";
 import "./App.css";
 
 class App extends Component {
     constructor() {
         super();
-        console.log("constructor");
         this.state = {
             monsters: [],
         };
     }
 
     componentDidMount() {
-        console.log("didmount");
         fetch(`https://jsonplaceholder.typicode.com/users`)
             .then((response) => response.json())
             .then((users) => this.setState({ monsters: users }));
     }
     render() {
-        console.log("render");
         return (
             <div className="App">
+                <CardList className="Armaan"></CardList>
                 {this.state.monsters.map((monster) => (
                     <h1 key={monster.id}>{monster.name}</h1>
                 ))}

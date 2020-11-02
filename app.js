b18a8eece8263b523fd9be0e6919056f00425d6f
armaan <armaanmathew88@gmail.com>
2020-11-02 21:07:17 +0530

    section 3 video 49

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // application
+
+// const app = () => {
+//     return React.createElement(
+//         "div",
+//         {},
+//         React.createElement("h1", {}, "React was Rendered Successfully!!"),
+//     );
+//     ReactDOM.render();
+// };

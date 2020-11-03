ec800064a9326c657c85488565a1d8212cfb8261
armaan <armaanmathew88@gmail.com>
2020-11-03 21:03:05 +0530

    section 3 51

---
+++ practice-app1/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            theYearThatSucks: 2020,
+        };
+    }
+    update = () => {
+        this.setState(
+            { theYearThatSucks: this.state.theYearThatSucks + 1 },
+            () => console.log(this.state.theYearThatSucks),
+        );
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>{this.state.theYearThatSucks}</p>
+                    <button onClick={this.update}>Update State</button>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;

6b8773a13770ac46d4bc16084912c1a5e1252407
armaan <armaanmathew88@gmail.com>
2020-11-11 00:53:12 +0530

    section 4 video 73 complete

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component.jsx";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => {
+    return (
+        <div>
+            <h1>HATS PAGE</h1>;
+        </div>
+    );
+};
 
 function App() {
     return (
         <div>
-            <Homepage></Homepage>
+            <Switch>
+                <Route path="/" component={Homepage}></Route>
+                <Route path="/hats" component={HatsPage}></Route>
+            </Switch>
         </div>
     );
 }

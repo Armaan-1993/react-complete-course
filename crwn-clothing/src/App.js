34b90b40d94dfc76ccaaa42f56cb1643f1bfbcaf
armaan <armaanmathew88@gmail.com>
2020-11-11 20:09:18 +0530

    working with section 5 video 75

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,25 +1,60 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./pages/homepage/homepage.component.jsx";
-import { Switch, Route } from "react-router-dom";
+// import Homepage from "./pages/homepage/homepage.component.jsx";
+import { Route } from "react-router-dom";
+// import Homepage from "./pages/homepage/homepage.component";
 
-const HatsPage = () => {
+// const HatsPage = () => {
+//     return (
+//         <div>
+//             <h1>HATS PAGE</h1>;
+//         </div>
+//     );
+// };
+
+// function App() {
+//     return (
+//         <div>
+//             <Switch>
+//                 <Route path="/" component={Homepage}></Route>
+//                 <Route path="/hats" component={HatsPage}></Route>
+//             </Switch>
+//         </div>
+//     );
+// }
+
+const HomePage = () => {
     return (
         <div>
-            <h1>HATS PAGE</h1>;
+            <h1>HOME PAGE</h1>
         </div>
     );
 };
 
-function App() {
+const TopicsList = () => {
     return (
         <div>
-            <Switch>
-                <Route path="/" component={Homepage}></Route>
-                <Route path="/hats" component={HatsPage}></Route>
-            </Switch>
+            <h1>TOPICS LIST</h1>
         </div>
     );
-}
+};
+
+const TopicDetail = () => {
+    return (
+        <div>
+            <h1>TOPIC DETAIL</h1>
+        </div>
+    );
+};
+
+const App = () => {
+    return (
+        <div>
+            <Route path="/" component={HomePage}></Route>
+            <Route exact path="/topics" component={TopicsList}></Route>
+            <Route exact path="/topics/topicId" component={TopicDetail}></Route>
+        </div>
+    );
+};
 
 export default App;

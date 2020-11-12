4fef0a37f1b8fbfad10a070a0306dc3d11fc7d5f
armaan <armaanmathew88@gmail.com>
2020-11-12 21:21:28 +0530

    section 5 video 75 completed

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 // import Homepage from "./pages/homepage/homepage.component.jsx";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 // import Homepage from "./pages/homepage/homepage.component";
 
 // const HatsPage = () => {
@@ -27,12 +27,16 @@ const HomePage = (props) => {
     console.log(props);
     return (
         <div>
+            <Link to={`${props.match.url}/13`}>Topics 13</Link>
+            <link to={`${props.match.url}/17`}>Topics 17</link>
+            <Link to={`${props.match.url}/19`}>Topics 19</Link>
             <h1>HOME PAGE</h1>
         </div>
     );
 };
 
-const TopicsList = () => {
+const TopicsList = (props) => {
+    console.log(props);
     return (
         <div>
             <h1>TOPICS LIST</h1>
@@ -40,10 +44,14 @@ const TopicsList = () => {
     );
 };
 
-const TopicDetail = () => {
+const TopicDetail = (props) => {
+    console.log(props);
     return (
         <div>
-            <h1>TOPIC DETAIL</h1>
+            <button onClick={() => props.history.push("topics")}>
+                Click me
+            </button>
+            <h1 id="mine">TOPIC DETAIL = {props.match.params.topicId}</h1>
         </div>
     );
 };
@@ -53,7 +61,7 @@ const App = () => {
         <div>
             <Route path="/" component={HomePage}></Route>
             <Route exact path="/topics" component={TopicsList}></Route>
-            <Route exact path="/topics/topicId" component={TopicDetail}></Route>
+            <Route path="/topics/:topicId" component={TopicDetail}></Route>
         </div>
     );
 };

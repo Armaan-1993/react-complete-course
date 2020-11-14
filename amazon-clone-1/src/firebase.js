bfe3e4e886489a85a1a3821d43d3e189ae0176ec
armaan <armaanmathew88@gmail.com>
2020-11-14 20:23:48 +0530

    firebase config

---
+++ amazon-clone-1/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwjm8aV_ivb8D_IdJvp8rIRMDOOz1X_EY",
+    authDomain: "clone-1-679c7.firebaseapp.com",
+    databaseURL: "https://clone-1-679c7.firebaseio.com",
+    projectId: "clone-1-679c7",
+    storageBucket: "clone-1-679c7.appspot.com",
+    messagingSenderId: "1060553267828",
+    appId: "1:1060553267828:web:9f81aa5642b543d3e74f21",
+    measurementId: "G-EDD6ZETN0Y",
+};

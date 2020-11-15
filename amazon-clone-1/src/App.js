4d49a29f9dcd8abcdc44b3e32b63ca47c5691f8d
armaan <armaanmathew88@gmail.com>
2020-11-16 00:53:26 +0530

    working on home component ui

--- amazon-clone-1/src/App.js
+++ amazon-clone-1/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header.component/header";
+import Home from "./components/home.component/home";
 
 function App() {
     return (
         <div className="App">
-            <Header></Header>
+            <Header />
+            <Home />
         </div>
     );
 }
---
+++ amazon-clone-1/src/components/home.component/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+    return (
+        <div>
+            <p>I am the Home Component</p>
+        </div>
+    );
+};
+
+export default Home;

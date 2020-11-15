c1cb20c49425c4e74b3e80720c1a649fd05dd866
armaan <armaanmathew88@gmail.com>
2020-11-15 20:10:18 +0530

    nav bar ui complete

--- amazon-clone-1/src/components/header.component/header.jsx
+++ amazon-clone-1/src/components/header.component/header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 const Header = () => {
     return (
@@ -9,10 +11,9 @@ const Header = () => {
                 src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
             />
             <div className="header_search">
-                <form>
-                    <input type="text" className="header_searchInput" />
-                    {/*logo*/}
-                </form>
+                <input type="text" className="header_searchInput" />
+                {/*logo*/}
+                <SearchIcon className="header_searchIcon"></SearchIcon>
             </div>
             <div className="header_navbar">
                 <div className="header_options">
@@ -26,6 +27,12 @@ const Header = () => {
                 <div className="header_options">
                     <span className="header_optionLineOne">Your</span>
                     <span className="header_optionLineTwo">Prime</span>
+                </div>
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon></ShoppingBasketIcon>
+                    <span className="header_optionLineTwo header_BasketCount">
+                        0
+                    </span>
                 </div>
             </div>
         </div>

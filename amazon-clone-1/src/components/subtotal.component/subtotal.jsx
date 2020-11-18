9d0482f0682c9e509c0a07ad71b70c32f7099adc
armaan <armaanmathew88@gmail.com>
2020-11-18 17:22:53 +0530

    subtotal component

--- amazon-clone-1/src/components/subtotal.component/subtotal.jsx
+++ amazon-clone-1/src/components/subtotal.component/subtotal.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import "./subtotal.css";
 import CurrencyFormat from "react-currency-format";
 
-function subtotal() {
-    return <div></div>;
-}
+const Subtotal = () => {
+    return (
+        <div className="subtotal">
+            <CurrencyFormat
+                renderText={(value) => (
+                    <>
+                        <p>
+                            Subtotal 0 items : <strong>0</strong>
+                        </p>
+                        <small className="subtotal_gift">
+                            <input type="checkbox" /> This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={0}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
 
-export default subtotal;
+            <button>Proceed to Checkout</button>
+        </div>
+    );
+};
+
+export default Subtotal;

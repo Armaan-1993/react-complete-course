585c44c0408f476d98cd9582bd10e07a74199c9e
armaan <armaanmathew88@gmail.com>
2020-11-20 23:31:56 +0530

    working on firebase user athentication for login , add new user and logout

--- amazon-clone-1/src/App.js
+++ amazon-clone-1/src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/header.component/header";
 import Home from "./components/home.component/home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Checkout from "./components/checkout.component/checkout";
 import Login from "./components/login.component/login";
+import { auth } from "./firebase";
 
 function App() {
+    useEffect(() => {
+        //useeffect will run once when the App component loads. its used to keep track of the users signed in
+        auth.onAuthStateChanged((authUser) => {
+            console.log(`The user is ${authUser}`);
+
+            if (authUser) {
+                //the user has logged in/was logged in
+            } else {
+                //the user is logged out
+            }
+        });
+    }, []);
+
     return (
         <Router>
             <div className="App">
--- amazon-clone-1/src/firebase.js
+++ amazon-clone-1/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBwjm8aV_ivb8D_IdJvp8rIRMDOOz1X_EY",
     authDomain: "clone-1-679c7.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
     appId: "1:1060553267828:web:9f81aa5642b543d3e74f21",
     measurementId: "G-EDD6ZETN0Y",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };

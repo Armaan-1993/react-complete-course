1d29a04ac7802111bd17ff350bda18888c8b6bdd
armaan <armaanmathew88@gmail.com>
2020-11-27 06:24:58 +0530

    section 6 complete

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop.component";
+import Header from "./components/header.component/header";
+import SignInAndSignUpPage from "./pages/sign-in and sign-up/sign-in-sign-up";
 
 function App() {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path="/" component={Homepage}></Route>
                 <Route path="/shop" component={ShopPage}></Route>
+                <Route path="/signin" component={SignInAndSignUpPage}></Route>
             </Switch>
         </div>
     );

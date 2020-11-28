192bd9472f3fd3c5e94585c123a44f37bba9de98
armaan <armaanmathew88@gmail.com>
2020-11-28 12:55:46 +0530

    working on sign up new user

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -5,7 +5,7 @@ import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop.component";
 import Header from "./components/header.component/header";
 import SignInAndSignUpPage from "./pages/sign-in and sign-up/sign-in-sign-up";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
     constructor() {
@@ -17,9 +17,21 @@ class App extends React.Component {
 
     unsubscribeFromAuth = null;
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-            this.setState({ currentUser: user });
-            console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+            // this.setState({ currentUser: user });
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+                userRef.onSnapshot((snapShot) => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data(),
+                        },
+                    });
+                    console.log(this.state);
+                });
+            }
+            this.setState({ currentUser: userAuth });
         });
     }
     componentWillUnmount() {

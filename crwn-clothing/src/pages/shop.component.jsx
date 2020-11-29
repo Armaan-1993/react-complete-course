448002ee7d52b8348471ad0c3e869824d608dd93
armaan <armaanmathew88@gmail.com>
2020-11-30 03:42:55 +0530

    section 11 complete

--- crwn-clothing/src/pages/shop.component.jsx
+++ crwn-clothing/src/pages/shop.component.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import CollectionsOverview from "../components/collections-overview/collections-overview";
+import CollectionPage from "../pages/collection/collection.component";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
     return (
         <div className="shop-page">
-            <CollectionsOverview />
+            <Route
+                exact
+                path={`${match.path}`}
+                component={CollectionsOverview}
+            ></Route>
+            <Route
+                path={`${match.path}/:collectionId`}
+                component={CollectionPage}
+            ></Route>
         </div>
     );
 };

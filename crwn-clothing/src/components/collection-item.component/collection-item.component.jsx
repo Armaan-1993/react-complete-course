cb6163da30012917fb819177132d024fc7130e0f
armaan <armaanmathew88@gmail.com>
2020-11-29 14:23:51 +0530

    working with cart items

--- crwn-clothing/src/components/collection-item.component/collection-item.component.jsx
+++ crwn-clothing/src/components/collection-item.component/collection-item.component.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
 import "./collection-item.component.scss";
+import CustomButton from "../custom-button-component/custom-button";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
     return (
         <div className="collection-item">
             <div
@@ -14,8 +18,14 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton onClick={() => addItem(item)} inverted>
+                Add to Cart
+            </CustomButton>
         </div>
     );
 };
+const mapDispatchToProps = (dispatch) => ({
+    addItem: (item) => dispatch(addItem(item)),
+});
 
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);

11558c17f07743310790ff4bdf6733a0c3284160
armaan <armaanmathew88@gmail.com>
2020-11-29 18:31:37 +0530

    added items to the cart list

--- crwn-clothing/src/components/cat-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cat-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import "./cart-component.styles.scss";
+import { connect } from "react-redux";
+
 import CustomButton from "../custom-button-component/custom-button";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () => {
-    return (
-        <div className="cart-dropdown">
-            <div className="cart-items"></div>
-            <CustomButton>GO TO CHECKOUT</CustomButton>
+import "../cat-dropdown/cart-component.styles.scss";
+
+const CartDropdown = ({ cartItems }) => (
+    <div className="cart-dropdown">
+        <div className="cart-items">
+            {cartItems.map((cartItem) => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
         </div>
-    );
-};
+        <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+);
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
